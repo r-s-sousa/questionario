f7c6ecac56259775673dff81c3a3cda66ad0cabb
Rafael <rafael_sousa2018@outlook.com>
2021-09-03 21:08:52 -0300

    thema incrementado da logo

--- themes/_theme.php
+++ themes/_theme.php
@@ -29,16 +29,9 @@
       <?= $this->section('navbar'); ?>
    </header>
 
-   <!-- LOGO DA UNB -->
-   <div class="container bg-white rounded-top" style="margin-top: 70px;">
-      <div class="row pt-3">
-         <div class="col-md-12 text-center">
-            <p>
-               <img src="<?= asset('imgs/logo.png'); ?>" alt="logo unb" class="img-fluid">
-            </p>
-         </div>
-      </div>
-   </div>
+   <section>
+      <?= $this->insert('main/partials/logoUnb'); ?> 
+   </section>
 
    <main>
       <?= $this->section('content'); ?>
